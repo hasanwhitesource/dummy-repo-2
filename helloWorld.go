6c059498d1a01ff752f2c10f4d99bd97fa6db810
Hasan Awad <90554456+hasanwhitesource@users.noreply.github.com>
2021-11-01 11:18:45 +0200

    Update helloWorld.go

--- helloWorld.go
+++ helloWorld.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/hasanwhitesource/dummy-repo-2/smith"
+)
 
 func main(){
 		fmt.Println("hello world")
+		smith.SmithPrinter("yoooooo")
 }
